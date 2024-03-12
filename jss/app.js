c3ccec64dd237f17f2256d4813d4e33f26115ca9
casalaspro <casalaspro.alessandro@gmail.com>
2024-03-12 14:48:27 +0100

    ADD - files index and js and made palindroma comments

---
+++ jss/app.js
@@ -0,0 +1,26 @@
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma:
+la funzione deve ritornare true se la parola è palindroma
+deve ritornare false se la parola non è palindroma
+alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+Consigli del giorno
+Scriviamo sempre in italiano i passaggi che vogliamo fare
+Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+*/
+
+// PALINDROMA
+// Creare una variambile che riceva un prompt con scritto "Inserire una parola:"
+// Creare una funzione che riceva stringa userString e la confronti con la sua copia invertita
+  // si renda minuscola la stringa
+  // variabile const revString = si applichi il metodo .toReversed() (metodo non distruttivo di .reverse())
+  // return userString === revString
+// Invocare la funzione con il risultato del prompt
+// Stampare con console.log() il risultato della funzione
